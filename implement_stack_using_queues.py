d15e04d02864be1b898eab2f7fa0972743654e8c
Kavana L <jayammakavanal@gmail.com>
2026-09-10 23:05:58 +0530

    Add implement stack using queues solution

---
+++ implement_stack_using_queues.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        for i in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+        return self.q.popleft()
+
+    def top(self) -> int:
+        for i in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+        ans = self.q.popleft()
+        self.q.append(ans)
+
+        return ans
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
